9d018e56c5dc55e595b3966bfa96b648a973f019
theft <gentledemeiz@gmail.com>
2023-12-03 07:09:04 +0300

    some reworks stat_dto

--- ewm-stat/stat_dto/src/main/java/ru/practicum/dto/StatResponseDto.java
+++ ewm-stat/stat_dto/src/main/java/ru/practicum/dto/StatResponseDto.java
@@ -5,17 +5,9 @@ import lombok.*;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class StatResponseDto {
-
     private String app;
-
     private String uri;
-
     private long hits;
-
-    public StatResponseDto(String app, String uri, long hits) {
-        this.app = app;
-        this.uri = uri;
-        this.hits = hits;
-    }
 }
